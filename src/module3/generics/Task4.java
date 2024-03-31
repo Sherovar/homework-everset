8d999463cb6da2e73b0dc5d8dd6bc4df33d02230
Sherovar <Sherovar@gmail.com>
2024-03-31 19:10:03 +0500

    Collection2 Stream API Generics

--- src/module3/generics/Task4.java
+++ src/module3/generics/Task4.java
@@ -7,10 +7,20 @@ import java.util.List;
 
 public class Task4 {
     public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+        List<String> strings = List.of("Red", "Green", "Orange");
+
+        System.out.println(getReversedList(integers));
+        System.out.println(getReversedList(strings));
+
 
     }
 
-/*    private static <K> void getReversedList(List<K> list){
-        Collections.sort(list,Comparator.reverseOrder());
-    }*/
+    private static <K> List<K> getReversedList(List<K> list){
+        ArrayList<K> ks = new ArrayList<>();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            ks.add(list.get(i));
+        }
+        return ks;
+    }
 }
